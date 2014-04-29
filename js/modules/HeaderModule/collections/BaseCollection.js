f96536af9e55d6aca566c74f837e936894afa4ba
Charlie Ginsberg <cginsberg0023@gmail.com>
2014-04-29 16:59:09 -0400

    base collection to extend off of to get test data by overriding fetch

---
+++ js/modules/HeaderModule/collections/BaseCollection.js
@@ -0,0 +1,38 @@
+/*global define */
+define(['backbone', 'marionette', 'app'], function(Backbone, Marionette, App) {
+	return Backbone.Collection.extend({
+		initialize: function(options) {},
+		
+		/**
+		 * Override collection fetch to provide stub data
+		 * @param  options 
+		 */
+		fetch: function(options) {
+			options = options || {};
+
+			var useStubData = App.Header.Config.USE_STUB_DATA || false,
+				stubPath = App.Header.Config.STUB_PATH || false,
+				stubFilename = this.constructor.stubFilename || false;
+
+
+			if (useStubData !== false && stubPath !== false && stubFilename !== false) {
+				var collection = this;
+
+				var xhr = $.ajax({
+					type: "GET",
+					dataType: "json",
+					url: stubPath + '/collections/' + stubFilename + '.json',
+					success: function(data) {
+						collection.reset(data);
+					}
+				});
+				
+				return xhr;
+			}
+
+			return Backbone.Collection.prototype.fetch.call(this, options);
+
+
+		}
+	});
+});
